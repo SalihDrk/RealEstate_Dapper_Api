bdf939c5ad0a306ba734836848d7dec32042b1b0
salih DURAK <salihdrk@outlook.com>
2023-10-17 23:35:07 +0300

    17.10.2023

---
+++ RealEstate_Dapper_Api/Repostories/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+
+namespace RealEstate_Dapper_Api.Repostories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync(); // kategori listeleme için
+
+        void CreateCategory(CreateCategoryDto categoryDto);  // kategori eklemek için
+
+        void DeleteCategory(int id);
+
+        void UpdateCategory(UpdateCategoryDto categoryDto);
+
+        Task <GetByIDCategoryDto> GetCategory(int id);
+    }
+}
